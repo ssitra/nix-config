f64c3ab7544a8d25c0489b8d64455d7f4d21b109
Artis Mucenieks <amucenieks@gmail.com>
2025-05-17 04:00:51 +0200

    Kissy face

--- modules/homepage/default.nix
+++ modules/homepage/default.nix
@@ -7,6 +7,7 @@
     services.homepage-dashboard = {
       allowedHosts = "*";
       openFirewall = true;
+      settings = {favicon = "https://fav.farm/😘";};
       services = [
         {
           "Media Management" = [
